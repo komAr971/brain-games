5c9ee681af999b0366adc6369281805abcf7c2cc
Konstantin Korostelev <korostel97@gmail.com>
2021-01-04 12:46:45 +0300

    add getRandomProgression function

--- src/random.js
+++ src/random.js
@@ -12,3 +12,24 @@ export const getRandomExpression = () => {
       return undefined;
   }
 };
+
+export const getRandomProgression = (
+  maxLengthProgression,
+  minLengthProgression,
+  maxFirstElement,
+  maxDifference,
+) => {
+  const resultProgression = [];
+
+  const lengthProgression = maxLengthProgression
+    + getRandomInt(maxLengthProgression - minLengthProgression + 1);
+  const firstElement = getRandomInt(maxFirstElement - 1) + 1;
+  const difference = getRandomInt(maxDifference - 1) + 1;
+
+  resultProgression.push(firstElement);
+  for (let i = 0; i < lengthProgression - 1; i += 1) {
+    resultProgression.push(resultProgression[i] + difference);
+  }
+
+  return resultProgression;
+};
